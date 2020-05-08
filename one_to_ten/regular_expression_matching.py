2b9b0503833ee2b179c35bef9f21205f2b9d9d2e
liang.li <liang.li@hex-tech.net>
2020-05-08 20:23:56 +0800

    add one to ten

---
+++ one_to_ten/regular_expression_matching.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        if not p:
+            return not s
+        first_match = bool(s) and (p[0]=='.' or s[0]==p[0])
+
+        if len(p) > 1 and p[1] == "*":
+            return self.isMatch(s, p[2:]) or (first_match and self.isMatch(s[1:], p))
+        else:
+            return first_match and self.isMatch(s[1:], p[1:])
+
+
+if __name__ == '__main__':
+    s = Solution().isMatch
+    s("aa", "a*")
