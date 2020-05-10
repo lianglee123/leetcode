b0511dfbdd4305f207e3bbd486f2695934767a8d
liang.li <liang.li@hex-tech.net>
2020-05-10 11:59:44 +0800

    [11~20]finish 3sum and 4sum

--- 11~20/3sum.py
+++ 11~20/3sum.py
@@ -5,11 +5,11 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         l = len(nums)
-        res = set()
+        res = set()  # 这种可以去重的方法是低效的，情况Solution2
         for i in range(l):
             j = i+1
             k = l-1
-            target = nums[i]
+            target = - nums[i]
             while k > j:
                 if nums[k]+nums[j] > target:
                     k -= 1
@@ -19,3 +19,34 @@ class Solution:
                     res.add((nums[i], nums[j], nums[k]))
         return list(res)
 
+
+class Solution2:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        length = len(nums)
+        res = []  # 这种可以去重的方法是低效的，情况Solution2
+        for i in range(length):
+            l = i+1
+            r = length-1
+            target = - nums[i]
+            while r > l:
+                s = nums[r]+nums[l]
+                if s > target:
+                    r -= 1
+                elif s < target:
+                    l += 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    while r > l and nums[r] == nums[r-1]:
+                        r -= 1
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    r -= 1
+                    l += 1
+        return res
+
+
+
+if __name__ == '__main__':
+    s = Solution2().threeSum
+    print(s([-6, 2, 2, 2, 4, 4, 4]))
---
+++ 11~20/4sum.py
@@ -0,0 +1,44 @@
+from typing import List
+
+
+class Solution:
+    def fourSum(self, nums, target):
+        nums.sort()
+        results = []
+        self.findNSum(nums, target, 4, [], results)
+        return results
+
+    def findNSum(self, nums, target, N, result: List[int], results: List[List[int]]):
+        if len(nums) < N or N < 2:
+            return
+        if N == 2:
+            l, r = 0, len(nums)-1
+            while l < r:
+                s = nums[l] + nums[r]
+                if s == target:
+                    results.append(result+[nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
+                    while r > l and nums[r] == nums[r+1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+                elif s < target:
+                    l += 1
+                else:
+                    r -= 1
+        else:
+            for i in range(0, len(nums)-N+1):
+                if target<nums[i]*N or target>nums[-1]*N:
+                    break
+                if i == 0 or i > 0 and nums[i-1] != nums[i]:
+                    self.findNSum(nums[i+1:], target-nums[i], N-1, result+[nums[i]], results)
+
+
+if __name__ == '__main__':
+    s = Solution().findNSum
+    a = [-6, 2, 2, 2, 4, 4, 4]
+    res = []
+    s(a, 0,4, [], res)
+    print(res)
+
