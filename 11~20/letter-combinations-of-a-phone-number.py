b50888eef512d0254271a3b64274adfa0779b90e
liang.li <liang.li@hex-tech.net>
2020-05-10 12:13:16 +0800

    [11~20]back trace solution to leeter-combinations-of-phone-number

--- 11~20/letter-combinations-of-a-phone-number.py
+++ 11~20/letter-combinations-of-a-phone-number.py
@@ -24,7 +24,40 @@ class Solution:
         return res
 
 
+class Solution2:
+    """
+    BackTrace
+    """
+    digit_map = {
+        '2': ['a', 'b', 'c'],
+        '3': ['d', 'e', 'f'],
+        '4': ['g', 'h', 'i'],
+        '5': ['j', 'k', 'l'],
+        '6': ['m', 'n', 'o'],
+        '7': ['p', 'q', 'r', 's'],
+        '8': ['t', 'u', 'v'],
+        '9': ['w', 'x', 'y', 'z'],
+    }
+
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+        self.back_trace('', digits, res)
+        return res
+
+    def back_trace(self, prefix, digits, res):
+        if not digits:
+            res.append(prefix)
+            return
+        for c in Solution2.digit_map[digits[0]]:
+            self.back_trace(prefix+c, digits[1:], res)
+
+
+
+
+
 
 if __name__ == '__main__':
-    s = Solution().letterCombinations
-    print(s("23456"))
+    s1 = Solution().letterCombinations
+    s2 = Solution2().letterCombinations
+
+    assert sorted(s1("23456")) == sorted(s2("23456"))
